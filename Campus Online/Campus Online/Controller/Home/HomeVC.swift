3659133c5f9683d0aa84969fcc468e79f7cbbb13
mhsnabz <mhsnabz@yahoo.com.tr>
2020-11-14 06:27:38 +0300

    ~

--- Campus Online/Campus Online/Controller/Home/HomeVC.swift
+++ Campus Online/Campus Online/Controller/Home/HomeVC.swift
@@ -689,11 +689,11 @@ extension HomeVC : UICollectionViewDelegate , UICollectionViewDelegateFlowLayout
         }
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 1
+        return 2
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
+        return 2
     }
     
     func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
